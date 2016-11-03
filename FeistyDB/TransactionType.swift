088cad6c12d172a324b96020992f314e19fe0508
Stephen F. Booth <me@sbooth.org>
2016-11-03 17:52:22 -0400

    Made TransactionType: CustomStringConvertible an extension

--- FeistyDB/TransactionType.swift
+++ FeistyDB/TransactionType.swift
@@ -8,14 +8,16 @@ import Foundation
 /// Possible transaction types
 ///
 /// - seealso: [Transactions in SQLite](https://sqlite.org/lang_transaction.html)
-public enum TransactionType: CustomStringConvertible {
+public enum TransactionType {
 	/// A deferred transaction
 	case deferred
 	/// An immediate transaction
 	case immediate
 	/// An exclusive transaction
 	case exclusive
+}
 
+extension TransactionType: CustomStringConvertible {
 	/// A description of the transaction type suitable for use in an SQL statement
 	public var description: String {
 		switch self {
