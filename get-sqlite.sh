db61bafea8e06f9c96667df45d1f2dd2308407de
Stephen F. Booth <me@sbooth.org>
2016-11-12 02:40:04 +0100

    Don't extract sqlite/shell.c

--- get-sqlite.sh
+++ get-sqlite.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 curl -O https://sqlite.org/2016/sqlite-amalgamation-3150100.zip
-unzip -ju sqlite-amalgamation-3150100.zip -d sqlite
+unzip -ju sqlite-amalgamation-3150100.zip -x "*/shell.c" -d sqlite
