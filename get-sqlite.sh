d3e3788ceceac9cb00f73fd0ab984a03cc670a82
Stephen F. Booth <me@sbooth.org>
2016-11-30 07:05:39 -0500

    Updated for SQLite 3.15.2

--- get-sqlite.sh
+++ get-sqlite.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -O https://sqlite.org/2016/sqlite-amalgamation-3150100.zip
-unzip -ju sqlite-amalgamation-3150100.zip -x "*/shell.c" -d sqlite
+curl -O https://sqlite.org/2016/sqlite-amalgamation-3150200.zip
+unzip -ju sqlite-amalgamation-3150200.zip -x "*/shell.c" -d sqlite
