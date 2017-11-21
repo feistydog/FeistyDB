0942ce2902e86b7a81cadde90d08ba8f2702b419
Stephen F. Booth <me@sbooth.org>
2017-11-21 09:45:24 -0600

    Added database backup functionality

--- FeistyDB/Database.swift
+++ FeistyDB/Database.swift
@@ -324,6 +324,44 @@ extension Database {
 }
 
 extension Database {
+	/// A callback for reporting the progress of a database backup.
+	///
+	/// - parameter remaining: The number of database pages left to copy
+	/// - parameter total: The total number of database pages
+	public typealias BackupProgress = (_ remaining: Int, _ total: Int) -> Void
+
+	/// Backs up the database to the specified URL.
+	///
+	/// - parameter url: The destination for the backup.
+	/// - parameter callback: An optional closure to receive progress information
+	///
+	/// - throws: An error if the backup could not be completed
+	///
+	/// - seealso: [Online Backup API](http://www.sqlite.org/c3ref/backup_finish.html)
+	/// - seealso: [Using the SQLite Online Backup API](http://www.sqlite.org/backup.html)
+	public func backup(to url: URL, progress callback: BackupProgress? = nil) throws {
+		let destination = try Database(url: url)
+
+		if let backup = sqlite3_backup_init(destination.db, "main", self.db, "main") {
+			var result: Int32
+			repeat {
+				result = sqlite3_backup_step(backup, 5)
+				callback?(Int(sqlite3_backup_remaining(backup)), Int(sqlite3_backup_pagecount(backup)))
+				if result == SQLITE_OK || result == SQLITE_BUSY || result == SQLITE_LOCKED {
+					sqlite3_sleep(250)
+				}
+			} while result == SQLITE_OK || result == SQLITE_BUSY || result == SQLITE_LOCKED
+
+			sqlite3_backup_finish(backup)
+
+			guard sqlite3_errcode(destination.db) == SQLITE_OK else {
+				throw DatabaseError(message: "Unable to backup database", takingDescriptionFromDatabase: destination.db)
+			}
+		}
+	}
+}
+
+extension Database {
 	/// A comparator for `String` objects.
 	///
 	/// - parameter lhs: The left-hand operand
