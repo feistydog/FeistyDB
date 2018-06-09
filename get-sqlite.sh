3c3706b6f93609c68710840826e78ac84f458e9a
Stephen F. Booth <me@sbooth.org>
2018-06-09 12:14:11 -0500

    Update SQLite to 3.24.0

--- get-sqlite.sh
+++ get-sqlite.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -O https://sqlite.org/2018/sqlite-amalgamation-3230100.zip
-unzip -ju sqlite-amalgamation-3230100.zip -x "*/shell.c" -d sqlite
+curl -O https://sqlite.org/2018/sqlite-amalgamation-3240000.zip
+unzip -ju sqlite-amalgamation-3240000.zip -x "*/shell.c" -d sqlite
