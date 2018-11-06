b849e97f01eb7f2f97cea9504fd374142403d72a
Stephen F. Booth <me@sbooth.org>
2018-11-05 21:57:48 -0600

    Updated SQLite to version 3.25.3

--- get-sqlite.sh
+++ get-sqlite.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -O https://sqlite.org/2018/sqlite-amalgamation-3250200.zip
-unzip -ju sqlite-amalgamation-3250200.zip -x "*/shell.c" -d sqlite
+curl -O https://sqlite.org/2018/sqlite-amalgamation-3250300.zip
+unzip -ju sqlite-amalgamation-3250300.zip -x "*/shell.c" -d sqlite
