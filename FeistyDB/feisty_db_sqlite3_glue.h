3ddfb1c2798e2ed0f15640444e5001230146f0a9
Stephen F. Booth <stephen@feisty-dog.com>
2020-06-19 08:15:29 -0500

    Added feisty_db_sqlite3_db_config_enable_load_extension()

--- FeistyDB/feisty_db_sqlite3_glue.h
+++ FeistyDB/feisty_db_sqlite3_glue.h
@@ -11,9 +11,12 @@
 /// Duplicates and returns `s` using memory allocated by `sqlite3_malloc()`
 char * feisty_db_sqlite3_strdup(const char *s);
 
-/// Equivalent to `sqlite3_vtab_config(d, SQLITE_VTAB_CONSTRAINT_SUPPORT, x)`
-int feisty_db_sqlite3_vtab_config_constraint_support(sqlite3 *d, int x);
-/// Equivalent to `sqlite3_vtab_config(d, SQLITE_VTAB_INNOCUOUS)`
-int feisty_db_sqlite3_vtab_config_innocuous(sqlite3 *d);
-/// Equivalent to `sqlite3_vtab_config(d, SQLITE_VTAB_DIRECTONLY)`
-int feisty_db_sqlite3_vtab_config_directonly(sqlite3 *d);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION, x, y)`
+int feisty_db_sqlite3_db_config_enable_load_extension(sqlite3 *db, int x, int *y);
+
+/// Equivalent to `sqlite3_vtab_config(db, SQLITE_VTAB_CONSTRAINT_SUPPORT, x)`
+int feisty_db_sqlite3_vtab_config_constraint_support(sqlite3 *db, int x);
+/// Equivalent to `sqlite3_vtab_config(db, SQLITE_VTAB_INNOCUOUS)`
+int feisty_db_sqlite3_vtab_config_innocuous(sqlite3 *db);
+/// Equivalent to `sqlite3_vtab_config(db, SQLITE_VTAB_DIRECTONLY)`
+int feisty_db_sqlite3_vtab_config_directonly(sqlite3 *db);
