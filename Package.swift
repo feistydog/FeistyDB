5976aa0d44d724bb6cd0ecf6019fd18f39d7d246
Jason Jobe <box2019@jasonjobe.com>
2020-06-28 11:40:28 -0400

    Added iOS and tvOS versions to Package

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
 	name: "FeistyDB",
 	platforms: [
 		.macOS(.v10_12),
+        .iOS(.v11), .tvOS(.v11),
 	],
 	products: [
 		// Products define the executables and libraries produced by a package, and make them visible to other packages.
