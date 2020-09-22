6f699da6a14434a0251b46b45fbe39756f5613e5
Stephen F. Booth <stephen@feisty-dog.com>
2020-09-22 17:38:09 -0400

    Add support for additional sqlite3_db_config() verbs

--- Sources/CSQLite/include/feisty_db_sqlite3_glue.h
+++ Sources/CSQLite/include/feisty_db_sqlite3_glue.h
@@ -11,8 +11,24 @@
 /// Duplicates and returns `s` using memory allocated by `sqlite3_malloc()`
 char * feisty_db_sqlite3_strdup(const char *s);
 
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_FKEY, x, y)`
+int feisty_db_sqlite3_db_config_enable_fkey(sqlite3 *db, int x, int *y);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_TRIGGER, x, y)`
+int feisty_db_sqlite3_db_config_enable_trigger(sqlite3 *db, int x, int *y);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_VIEW, x, y)`
+int feisty_db_sqlite3_db_config_enable_view(sqlite3 *db, int x, int *y);
 /// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION, x, y)`
 int feisty_db_sqlite3_db_config_enable_load_extension(sqlite3 *db, int x, int *y);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_NO_CKPT_ON_CLOSE, x, y)`
+int feisty_db_sqlite3_db_config_no_ckpt_on_close(sqlite3 *db, int x, int *y);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_QPSG, x, y)`
+int feisty_db_sqlite3_db_config_enable_qpsg(sqlite3 *db, int x, int *y);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_DEFENSIVE, x, y)`
+int feisty_db_sqlite3_db_config_defensive(sqlite3 *db, int x, int *y);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_WRITABLE_SCHEMA, x, y)`
+int feisty_db_sqlite3_db_config_writable_schema(sqlite3 *db, int x, int *y);
+/// Equivalent to `sqlite3_db_config(db, SQLITE_DBCONFIG_TRUSTED_SCHEMA, x, y)`
+int feisty_db_sqlite3_db_config_trusted_schema(sqlite3 *db, int x, int *y);
 
 /// Equivalent to `sqlite3_vtab_config(db, SQLITE_VTAB_CONSTRAINT_SUPPORT, x)`
 int feisty_db_sqlite3_vtab_config_constraint_support(sqlite3 *db, int x);
