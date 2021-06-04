12f36a1e4991bfa7d5890009db8a1632d9866208
Stephen F. Booth <stephen@feisty-dog.com>
2021-06-04 08:33:43 -0500

    Refactor CSQLite

---
+++ Sources/CFeistyDB/empty.c
@@ -0,0 +1,4 @@
+
+// Workaround the Swift Package Manager requirement to have at least one
+// source file per package
+// See https://forums.swift.org/t/header-only-library-using-swift-package-manager/42700
