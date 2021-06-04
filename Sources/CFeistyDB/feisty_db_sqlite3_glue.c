4ab427ecbfca81631990edc292556c5d3c786ca0
Stephen F. Booth <stephen@feisty-dog.com>
2021-06-04 12:23:10 -0500

    Rename empty.c

--- Sources/CFeistyDB/feisty_db_sqlite3_glue.c
+++ Sources/CFeistyDB/feisty_db_sqlite3_glue.c
@@ -2,3 +2,8 @@
 // Workaround the Swift Package Manager requirement to have at least one
 // source file per package
 // See https://forums.swift.org/t/header-only-library-using-swift-package-manager/42700
+
+static void feisty_db_initialize(void) __attribute__ ((constructor));
+static void feisty_db_initialize()
+{
+}
