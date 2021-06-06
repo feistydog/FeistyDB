9ba834f161cbf61fd8fe347b5247f32ebd58ad0c
Stephen F. Booth <stephen@feisty-dog.com>
2021-06-06 07:51:04 -0500

    Don't perform automatic SQLite initialization

--- Sources/CFeistyDB/empty.c
+++ Sources/CFeistyDB/empty.c
@@ -1,20 +1,4 @@
-//
-// Copyright (c) 2021 Feisty Dog, LLC
-//
-// See https://github.com/feistydog/FeistyDB/blob/master/LICENSE.txt for license information
-//
 
-#include "csqlite_shims.h"
-
-static void feisty_db_initialize(void) __attribute__ ((constructor));
-static void feisty_db_initialize()
-{
-	// It's necessary to call sqlite3_initialize() since SQLITE_OMIT_AUTOINIT is defined in CSQLite
-	sqlite3_initialize();
-	csqlite_sqlite3_auto_extension_carray();
-	csqlite_sqlite3_auto_extension_decimal();
-	csqlite_sqlite3_auto_extension_ieee754();
-	csqlite_sqlite3_auto_extension_series();
-	csqlite_sqlite3_auto_extension_sha3();
-	csqlite_sqlite3_auto_extension_uuid();
-}
+// Fulfill the Swift Package Manager requirement to have at least one
+// source file per package
+// See https://forums.swift.org/t/header-only-library-using-swift-package-manager/42700
