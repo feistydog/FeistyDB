e240afef977306bac65b5315c0d26e6eba057cd8
Stephen F. Booth <stephen@feisty-dog.com>
2023-09-28 13:17:33 -0500

    Tweak formatting

--- Package.swift
+++ Package.swift
@@ -31,7 +31,8 @@ let package = Package(
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
 			name: "CFeistyDB",
-			dependencies: ["CSQLite"]),
+			dependencies: ["CSQLite"]
+		),
 		.target(
 			name: "FeistyDB",
 			dependencies: ["CFeistyDB"]
