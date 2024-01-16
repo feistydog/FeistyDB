9d9879ce2ba131a6050e473e0015af29b24719be
Stephen F. Booth <stephen@feisty-dog.com>
2024-01-16 09:09:36 -0600

    Update to oldest support version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ import PackageDescription
 let package = Package(
 	name: "FeistyDB",
 	platforms: [
-		.macOS(.v10_12),
+		.macOS(.v10_13),
 	],
 	products: [
 		// Products define the executables and libraries produced by a package, and make them visible to other packages.
